8deb6c7559180e8b7706ed4e794c33d4e42cb3a3
김은서 <es.kim2@okestro.com>
2025-07-24 10:42:12 +0900

    [programmers] 42885. 구명보트

---
+++ eunseo/programmers/42885.js
@@ -0,0 +1,16 @@
+// [programmers] 42885. 구명보트
+
+function solution(people, limit) {
+  var answer = 0;
+  people.sort((a,b) => a - b);
+  let i = 0;
+  let j = people.length - 1;
+  while(i <= j) {
+      if (people[i] + people[j] <= limit) {
+          i++;            
+      }
+      j--;
+      answer += 1;
+  }
+  return answer;
+}
